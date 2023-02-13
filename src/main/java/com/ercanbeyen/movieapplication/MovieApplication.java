cbb4c8909a3be991815c54e6dfad5389e45101ee
Ercan Beyen <96631352+ercanbeyen@users.noreply.github.com>
2023-02-13 13:54:51 +0300

    Add Open Api

--- src/main/java/com/ercanbeyen/movieapplication/MovieApplication.java
+++ src/main/java/com/ercanbeyen/movieapplication/MovieApplication.java
@@ -1,12 +1,30 @@
 package com.ercanbeyen.movieapplication;
 
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Blogging Platform",
+				version = "1.0",
+				description = "Blog API",
+				contact = @Contact(
+						name = "Blog API Support",
+						email = "support@email.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "http://www.apache.org/licenses/LICENSE-2.0.html"
+				)
+		))
 public class MovieApplication {
 
 	public static void main(String[] args) {
